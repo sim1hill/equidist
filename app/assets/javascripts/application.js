1a4d6d8536db387cbc9b57c69587fec8ae9af3e5
sim1hill <simone.c.hill@gmail.com>
2015-07-06 21:31:05 -0700

    map on the page centered at correct coordinates

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,49 +16,63 @@
 //= require_tree ../../../vendor/assets/javascripts/.
 //= require_tree .
 
-
-$(function($) {
-  if ($("#map-canvas").length > 0){
-    // load google maps api
-    var script = document.createElement('script');
-    script.src = "//maps.googleapis.com/maps/api/js?sensor=false&callback=initialize";
-    document.body.appendChild(script);
-  }
-});
-
-// var url = window.location.href + "/marker.json";
-
-function initialize() {
     
-  // var markerApi = function(){
-  //   return $.ajax({
-  //   type: "GET",
-  //   url: url,
-  //   dataType: "json"
-  //   });
-  // }
 
-  var drawMap = markerApi().done(
-    function(data){
-      // var lat = data["marker"]["latitude"];
-      // var lon = data["marker"]["longitude"];
-      var coordinates = new google.maps.LatLng(40.7048872, -74.0123737);
-      var map;
-      var mapOptions = {
-      center: coordinates,
-      zoom: 15,
-      disableDefaultUI: true,
-      styles: styles
-    };
-      
-    map = new google.maps.Map(document.getElementById("map-canvas"),
-mapOptions);
 
-    marker = new google.maps.Marker({
-      position: coordinates,
-      map: map,
-    });
 
-  });
 
+
+ function initialize() {
+    var latitude;
+    var longitude;
+    var latlng;
+    var mapOptions;
+    var map;
+
+    function getLocation() {
+        navigator.geolocation.getCurrentPosition(showPosition);
+   
+    }
+    getLocation();
+
+    function showPosition(position) {
+      latitude = position["coords"]["latitude"];
+        longitude = position["coords"]["longitude"];
+        // latitude = Number(position["coords"]["latitude"].toFixed(2));
+        // longitude = Number(position["coords"]["longitude"].toFixed(2));
+      latlng = new google.maps.LatLng(latitude, longitude);
+      mapOptions = {
+          zoom: 8,
+          center: latlng,
+  };
+        map = new google.maps.Map(document.getElementById('map-canvas'),
+            mapOptions);
+      }
 }
+
+  
+  
+
+function loadScript() {
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp' +
+      '&signed_in=true&callback=initialize';
+  document.body.appendChild(script);
+}
+
+window.onload = loadScript;
+
+// $(function($) {
+//   if ($("#map-canvas").length > 0){
+//     // load google maps api
+//     var script = document.createElement('script');
+//     script.src = "//maps.googleapis.com/maps/api/js?sensor=false&callback=initialize";
+//     document.body.appendChild(script);
+//   }
+// });
+
+
+
+
+
--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -11,11 +11,11 @@ class MapController < ApplicationController
   end
 
 def get_map
-    @marker = [40.7048872,-74.0123737]
-    # @marker = [request.location.latitude, request.location.longitude]
-    respond_to do |format|
-      format.json {render :marker}
-    end
+    # @marker = [40.7048872,-74.0123737]
+    # # @marker = [request.location.latitude, request.location.longitude]
+    # respond_to do |format|
+    #   format.json {render :marker}
+    # end
 end
 
 def create
